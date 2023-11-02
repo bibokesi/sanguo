16bf609f46390f92e01bc4dc91cce0a3bb4d2b72
zhTestTest <283809941@qq.com>
2023-11-02 10:30:39 +0800

    修复打包后真机环境热更报错missing aot

--- Assets/HybridCLRData/Editor/Custom/AOTMetaAssembliesHelper.cs
+++ Assets/HybridCLRData/Editor/Custom/AOTMetaAssembliesHelper.cs
@@ -33,27 +33,27 @@ public static class AOTMetaAssembliesHelper
     }
     public static void FindAllAOTMetaAssemblies(BuildTarget buildTarget)
     {
-//        fixed by zh
-//        string folder = $"{SettingsUtil.GetAssembliesPostIl2CppStripDir(buildTarget)}";
-//        GameMainSettingsUtils.GameMainHybridCLRSettings.AOTMetaAssemblies.Clear();
-//        if (!Directory.Exists(folder))
-//        {
-//#if UNITY_EDITOR_WIN
-//            Logger.Error($"AOTMetaAssemblies文件夹不存在，因此需要你先在菜单栏中(HybridCLR>>Generate>>All)操作。FolderPath:{folder}");
-//#elif UNITY_EDITOR_OSX
-//            Logger.Error($"AOTMetaAssemblies文件夹不存在，请检查是否制作UnityEditor.CoreModule.dll,并修改覆盖Unity安装路径，然后需要你先在菜单栏中(HybridCLR>>Generate>>All)操作。FolderPath:{folder}");
-//#endif
-//            return;
-//        }
-//        DirectoryInfo root = new DirectoryInfo(folder);
-//        foreach (var fileInfo in root.GetFiles("*dll", SearchOption.AllDirectories))
-//        {
-//            string fileName = fileInfo.Name;
-//            GameMainSettingsUtils.GameMainHybridCLRSettings.AOTMetaAssemblies.Add(fileName);
-//        }
-//        EditorUtility.SetDirty(GameMainSettingsUtils.GameMainHybridCLRSettings);
-//        AssetDatabase.SaveAssets();
-//        AssetDatabase.Refresh();
+        //        fixed by zh
+        string folder = $"{SettingsUtil.GetAssembliesPostIl2CppStripDir(buildTarget)}";
+        GameMainSettingsUtils.GameMainHybridCLRSettings.AOTMetaAssemblies.Clear();
+        if (!Directory.Exists(folder))
+        {
+#if UNITY_EDITOR_WIN
+            Logger.Error($"AOTMetaAssemblies文件夹不存在，因此需要你先在菜单栏中(HybridCLR>>Generate>>All)操作。FolderPath:{folder}");
+#elif UNITY_EDITOR_OSX
+            Logger.Error($"AOTMetaAssemblies文件夹不存在，请检查是否制作UnityEditor.CoreModule.dll,并修改覆盖Unity安装路径，然后需要你先在菜单栏中(HybridCLR>>Generate>>All)操作。FolderPath:{folder}");
+#endif
+            return;
+        }
+        DirectoryInfo root = new DirectoryInfo(folder);
+        foreach (var fileInfo in root.GetFiles("*dll", SearchOption.AllDirectories))
+        {
+            string fileName = fileInfo.Name;
+            GameMainSettingsUtils.GameMainHybridCLRSettings.AOTMetaAssemblies.Add(fileName);
+        }
+        EditorUtility.SetDirty(GameMainSettingsUtils.GameMainHybridCLRSettings);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
 
     public static void FindAllAOTMetaAssemblies1(BuildTarget buildTarget)
