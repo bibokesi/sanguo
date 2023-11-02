830db4224e9ace1a9c860e32301ebc75b45b4e6c
zhTestTest <283809941@qq.com>
2023-11-02 16:40:10 +0800

    test

--- Assets/GameMain/Scripts/Hotfix/GameMainExample1/Definition/Constant/EventName.cs
+++
@@ -1,9 +0,0 @@
-public static class AGameMainEventName
-{
-    #region UI 50000-59999
-    public const uint EVENT_GAME_ENTER_GAME_VIDEO = 0x050000;
-    public const uint EVENT_GAME_OUT_GAME_VIDEO = 0x050001;
-
-    
-    #endregion
-}
